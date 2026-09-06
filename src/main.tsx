5a11df619a3eb17ea66ab994aa85e550b841bfbb
ssakthivel02 <ssakthivel02@gmail.com>
2026-09-07 00:12:34 +0100

    feat(content): load KVS production supplement at runtime

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './content/registerSupplementalQuizzes'
+import './content/registerKvsProductionSupplement'
 import App from './app/App'
 import './app/index.css'
 import './app/print.css'
